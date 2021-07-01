b1296a6cdb5d6efe8dab1dc72989029cc21e5c71
15384021098 <15384021098@qq.com>
2021-07-01 22:07:15 +0800

    后台

---
+++ admin_menu/resources/views/partials/head.blade.php
@@ -0,0 +1,42 @@
+<meta charset="utf-8">
+<title>
+    {{ trans('global.global_title') }}
+</title>
+
+<meta http-equiv="X-UA-Compatible"
+      content="IE=edge">
+<meta content="width=device-width, initial-scale=1.0"
+      name="viewport"/>
+<meta http-equiv="Content-type"
+      content="text/html; charset=utf-8">
+
+<!-- Tell the browser to be responsive to screen width -->
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
+<!-- Ionicons -->
+<link rel="stylesheet" href="{{asset('css/ionicons.min.css')}}">
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="{{asset('js/html5shiv.min.js')}}"></script>
+<script src="{{asset('js/respond.min.js')}}"></script>
+<![endif]-->
+
+<link href="{{ url('adminlte/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+<link rel="stylesheet"
+      href="{{ url('adminlte/css') }}/select2.min.css"/>
+<link href="{{ url('adminlte/css/AdminLTE.min.css') }}" rel="stylesheet">
+<link href="{{ url('adminlte/css/skins/skin-blue.min.css') }}" rel="stylesheet">
+<link rel="stylesheet"
+      href="{{asset('css/jquery-ui.css')}}">
+<link rel="stylesheet"
+      href="{{asset('css/jquery.dataTables.min.css')}}"/>
+<link rel="stylesheet"
+      href="{{asset('css/select.dataTables.min.css')}}"/>
+<link rel="stylesheet"
+      href="{{asset('css/buttons.dataTables.min.css')}}"/>
+<link rel="stylesheet"
+      href="{{asset('css/jquery-ui-timepicker-addon.min.css')}}"/>
+<link rel="stylesheet"
+      href="{{asset('css/bootstrap-datepicker.standalone.min.css')}}"/>
